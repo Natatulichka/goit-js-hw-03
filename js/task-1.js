e2253105a0fbc1d41f89383ea795e1e679d64d00
Natalia Huliaieva <155992841+Natatulichka@users.noreply.github.com>
2024-04-20 14:47:20 +0200

    hw-3

--- js/task-1.js
+++ js/task-1.js
@@ -1,16 +1,13 @@
-// Замовлення дроїдів
-
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  let totalCost = quantity * pricePerDroid;
-  if (totalCost > customerCredits) {
-    return "Insufficient funds!";
-  } else {
-    return `You ordered ${quantity} droids worth ${totalCost} credits!`;
+// Генератор slug
+function slugify(title) {
+  let slug = "";
+  for (let i = 0; i < title.length; i++) {
+    title[i] === " " ? (slug += "-") : (slug += title[i].toLowerCase());
   }
+  return slug;
 }
 
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
--- js/task-2.js
+++ js/task-2.js
@@ -1,14 +1,18 @@
-// Форматування повідомлення
-function formatMessage(message, maxLength) {
-  if (message.length <= maxLength) {
-    return message;
-  } else {
-    return message.substring(0, maxLength) + "...";
+// Композиція масивів
+function makeArray(firstArray, secondArray, maxLength) {
+  let array = [];
+  for (let i = 0; i < maxLength; i++) {
+    if (i < firstArray.length) {
+      array.push(firstArray[i]);
+    } else {
+      array.push(secondArray[i - firstArray.length]);
+    }
   }
+  return array;
 }
-console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
