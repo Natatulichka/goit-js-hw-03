44cac3b4225ac90c4ab46e1d152a69ffd0156a04
Natalia Huliaieva <155992841+Natatulichka@users.noreply.github.com>
2024-04-21 16:24:34 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,14 +1,11 @@
 // Композиція масивів
 function makeArray(firstArray, secondArray, maxLength) {
-  let array = [];
-  for (let i = 0; i < maxLength; i++) {
-    if (i < firstArray.length) {
-      array.push(firstArray[i]);
-    } else {
-      array.push(secondArray[i - firstArray.length]);
-    }
+  let array = firstArray.concat(secondArray);
+  if (array.length > maxLength) {
+    return array.slice(0, maxLength);
+  } else {
+    return array;
   }
-  return array;
 }
 console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
 console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
